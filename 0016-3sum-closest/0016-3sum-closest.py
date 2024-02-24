c740af53c72d77069edfacef2aaa642ffab45412
Yeji Yoon <62207156+yjyg1215@users.noreply.github.com>
2024-02-24 16:19:57 +0900

    Time: 511 ms (40.10%), Space: 11.7 MB (83.26%) - LeetHub

---
+++ 0016-3sum-closest/0016-3sum-closest.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def threeSumClosest(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        nums.sort()
+        result = nums[0] + nums[1] + nums[2]
+        for i in range(len(nums) - 2):
+            l, r = i+1, len(nums) - 1
+            while l < r:
+                sum = nums[i] + nums[l] + nums[r]
+                if sum == target:
+                    return sum
+                if abs(sum - target) < abs(result - target):
+                    result = sum
+                if sum < target:
+                    l += 1
+                elif sum > target:
+                    r -= 1
+            
+        return result
